52be12df4c5a6ebdb2802fc2f140f9873a36535a
tariqul420 <tariqul.developer@gmail.com>
2025-04-29 16:09:58 +0600

    Implement Express server with JWT authentication and MongoDB connection

---
+++ index.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
+const userHandler = require('./router/userHandler');
+const dbConnect = require('./lib/dbConnect');
+
+// Express app initialization
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+// Middleware
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// Connect to MongoDB
+dbConnect();
+
+// Create a JWT token
+app.post('/jwt', async (req, res) => {
+  try {
+    const userInfo = req.body;
+    const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true });
+  } catch (err) {
+    res.status(500).send({
+      error: 'There was a server-side error',
+      details: err.message,
+    });
+  }
+});
+
+// Logout route
+app.get('/logout', async (req, res) => {
+  try {
+    res
+      .clearCookie('kutto_Token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        path: '/',
+      })
+      .send({ success: true });
+  } catch (err) {
+    res.status(500).send({
+      error: 'There was a server-side error',
+      details: err.message,
+    });
+  }
+});
+
+// Application routes
+app.use('/user', userHandler);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('This Server For MyAwesomeApp Website ❤️');
+});
+
+app.listen(port, () => {
+  console.log(`☘️  You successfully connected to Server: ${port}`);
+});
